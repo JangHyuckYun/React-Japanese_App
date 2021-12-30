1143fe6087a534e718eddfc19192f3ddb3b90071
by09019 <skg09203@naver.com>
2021-12-30 12:13:59 +0900

    modify

--- README.md
+++ README.md
@@ -9,6 +9,6 @@ Link : `https://by09019.github.io/React-Japanese_App/`
 > 친구가 다니는 일본어 학원은 주로 책을 이용해 공부를 합니다. 하지만 대중교통 등 다른곳에서도 책으로 공부하긴 불편하여
 > 앱,웹으로 만들어달라고 부탁받아 개발을 하게 되었습니다.
 > 
-> 크게 단어(Words), 문법(Grammers), EJU 이 3개로 나뉘어 집니다.
+> 크게 단어(Words), 문법(Grammers), EJU(Examination for Japanese University) 이 3개로 나뉘어 집니다.
 > 
 > ...
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import {BrowserRouter as Router, Route, Routes, useLocation, } from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, useLocation,} from "react-router-dom";
 import {ThemeProvider} from "styled-components";
 import {darkTheme, lightTheme} from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
@@ -15,15 +15,16 @@ import data from "./json/data.js";
 
 const App = () => {
     const [theme, setTheme] = useState(lightTheme);
-    console.log(Location);
     const switchTheme = () => {
         const nextTheme = theme === lightTheme ? darkTheme : lightTheme;
         setTheme(nextTheme);
     }
-    const { href } = window.location;
-    if(href.split("/")[href.split("/").length - 1] === process.env.PUBLIC_URL.split("/")[1]) {
-        window.location.href = href + "/home";
+    const {href} = window.location;
+    const checkUrl = process.env.PUBLIC_URL.split("/")[1];
+    if ((href.split("/")[href.split("/").length - 1] === "" && href.split("/")[href.split("/").length - 2] === checkUrl) || href.split("/")[href.split("/").length - 1] === checkUrl) {
+        window.location.href = href + (href.split("/")[href.split("/").length - 1] === "" ? "" : "/") + "home";
     }
+    console.log(href, href.split("/"), process.env.PUBLIC_URL.split("/")[1]);
 
     return (
         <ThemeProvider theme={theme}>
@@ -33,7 +34,7 @@ const App = () => {
                     <Container>
                         <Header/>
                         <Routes>
-                            <Route path={"/home"} element={<Home />}/>
+                            <Route path={"/home"} element={<Home/>}/>
                             <Route path="/learn" element={<Learns/>}/>
                             <Route path="/learn/:type" element={<Learn allData={data.data.language.jp}/>}/>
                             <Route path="/settings" element={<Settings switchTheme={() => switchTheme}/>}/>
