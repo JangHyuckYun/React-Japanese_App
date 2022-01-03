44d438e8ade58628a80feb54cfb56806911e21d2
by09019 <skg09203@naver.com>
2022-01-03 23:19:41 +0900

    add info json

---
+++ src/json/info.js
@@ -0,0 +1,38 @@
+import menuCherryBlossom from "../images/menu-cherryblossom.png";
+import menuPheasant from "../images/menu-pheasant.png";
+import menuCat from "../images/menu-cat.png";
+export default {
+    "level": {
+        "N1": {text: ""},
+        "N2": {text: ""},
+        "N3": {text: ""},
+        "N4": {text: ""},
+        "N5": {text: ""}
+    },
+    "learns": {
+        "words": {
+            "name": "Words",
+            "text": "this is test lines...",
+            "background": "#EB7662",
+            "backgroundImageUrl": menuCherryBlossom
+        },
+        "grammars": {
+            "name": "Grammars",
+            "text": "this is test lines...",
+            "background": "#8DC4BB",
+            "backgroundImageUrl": menuCherryBlossom
+        },
+        "eju": {
+            "name": "EJU",
+            "text": "this is test lines...",
+            "background": "#F2982F",
+            "backgroundImageUrl": menuCherryBlossom
+        },
+        "settings": {
+            "name": "Settings",
+            "text": "this is test lines...",
+            "background": "#327389",
+            "backgroundImageUrl": menuCherryBlossom
+        }
+    }
+};
