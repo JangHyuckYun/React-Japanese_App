e3b36df49c84f2843802581b5041f006677cc407
by09019 <skg09203@naver.com>
2022-01-18 18:08:59 +0900

    * TODO 추가

--- src/Level.js
+++ src/Level.js
@@ -3,7 +3,11 @@ import styledLevelComponents from "./styles/Level-Style";
 import {Link, useParams, useLocation, use} from "react-router-dom";
 
 const {MenusStyle, Level} = styledLevelComponents;
-// eslint-disable-next-line react-hooks/rules-of-hooks
+/*
+TODO
+ - props로 데이터 옮기지 말고 infoStore 활용하여 json 옮기기
+ - chapter 형태로 꾸미기 (chapter 안에 word들 표시)
+*/
 const Learn = (props) => {
     const { type, level } = useParams();
     const { allData } = props;
