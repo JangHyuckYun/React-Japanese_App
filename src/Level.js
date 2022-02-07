89c463d42d81067a90d70b0a71268d70998d6225
by09019 <skg09203@naver.com>
2022-02-07 15:43:19 +0900

    애니메이션 기능 수정

--- src/Level.js
+++ src/Level.js
@@ -4,66 +4,117 @@ import {Link, useParams, useLocation} from "react-router-dom";
 import {InfoContext} from "./contexts/info";
 import useColor from "./customHooks/useColor";
 import ContentByChapter from "./ContentByChapter";
+import {useAnimation, useMotionValue, useViewportScroll} from "framer-motion";
 
 const {MenusStyle, LevelItem} = styledLevelComponents;
 /*
 TODO
- - props로 데이터 옮기지 말고 infoStore 활용하여 json 옮기기
- - chapter 형태로 꾸미기 (chapter 안에 word들 표시)
 */
+const getDuration = (idx) => {
+    const maxDuration = 0.3;
+    const addDuration = 0.1 * (idx + 1);
+
+    return addDuration > maxDuration ? maxDuration : addDuration;
+}
+
 const Level = (props) => {
     const {type, level} = useParams();
-    const {data: {language}, profile: {language: {study}} } = useContext(InfoContext);
+    const {data: {language}, profile: {language: {study}}} = useContext(InfoContext);
     const data = language[study][type][level];
-    const { pathname } = useLocation();
-    const { getRandomColor } = useColor();
     const levelRef = useRef([]);
+    const container = useRef(0);
+    const containerScrollY = useMotionValue(0);
     let testData = Array.from(Array(15));
     let [dataClickInfo, setDataClickInfo] = useState(testData.slice().map((d, idx) => {
         return {
-          isClick:false,
+            isClick: false,
+            isShow: false,
+            isAnimation:false,
         };
     }));
 
     const clickLevel = ($event) => {
-        const { dataset: { idx } } = $event.target;
-        let modifyDataClickInfo = dataClickInfo.map((d, dIdx) => {
+        const {target , target: { dataset: {idx} }} = $event;
 
-            return {
-                isClick:d.isClick ? false : (Number(idx) === dIdx)
-            }
-        });
+        if(target.className.includes("chapterButton")) {
+            let modifyDataClickInfo = dataClickInfo.map((d, dIdx) => {
+                let isShow = d.isShow;
+                let isClick = d.isClick;
+                let isAnimation = d.isAnimation;
 
-        setDataClickInfo(modifyDataClickInfo);
+                isClick = isClick ? false : (Number(idx) === dIdx);
+
+                if(isClick) {
+                    isAnimation = true;
+                    isShow = true;
+                } else if(isShow && isAnimation) {
+                    isShow = !isShow;
+                } else if(!isShow && isAnimation) {
+                    isAnimation = !isAnimation;
+                }
+
+                return {
+                    isShow,
+                    isClick,
+                    isAnimation
+                }
+            });
+            console.log("modifyDataClickInfo", modifyDataClickInfo);
+            // 한개라도 클릭된 상태일 시 스크롤 안되도록 설정
+            const isClicked = modifyDataClickInfo.filter(item => item.isClick).length > 0;
+            container.current.style.overflow = isClicked ? "hidden" : "auto";
+
+            containerScrollY.set(getContainerScrollTop());
+            setDataClickInfo(modifyDataClickInfo);
+        }
     }
     const levelOffsetTop = levelRef.current.slice().map(ref => {
         return ref.offsetTop
     });
 
+    const getContainerScrollTop = () => {
+        return container.current !== undefined ? container.current.scrollTop : 0;
+    }
+
     return (
-        <MenusStyle>
+        <MenusStyle ref={el => container.current = el}>
             {testData.slice().map((d, idx) => {
                 return <LevelItem
+                    className={"chapterButton"}
                     key={idx}
                     ref={el => (levelRef.current[idx] = el)}
                     backgroundcolor={"#8dc4bb"}
                     isClick={dataClickInfo[idx].isClick}
                     data-idx={idx}
+                    zIndex={dataClickInfo[idx].isClick ? 999 : 1}
+                    custom={idx}
                     onClick={($event) => clickLevel($event)}
                     animate={dataClickInfo[idx].isClick ? "open" : "close"}
                     variants={{
-                        open: {
-                            translateY: ["0px", `-${levelOffsetTop[idx] - 10}px`],
+                        open:{
+                            translateY: ["0px", `-${levelOffsetTop[idx] - 10 - containerScrollY.get()}px`],
+                            zIndex: 999
                         },
-                        close: {
+                        close:{
+                            transition:{
+                                delay:getDuration(idx)
+                            },
                             translateY: 0,
+                            zIndex: 1,
                         }
                     }}
                     transition={{
-                        duration: .3
+                        duration: getDuration(idx)
                     }}
-                ><span>Chapter{idx + 1}</span>
-                    {dataClickInfo[idx].isClick ? <ContentByChapter chapterData={data[idx] || []} /> : ""}
+                ><span className={"chapterButton"} data-idx={idx}>Chapter{idx + 1}</span>
+                    <ContentByChapter
+                        className={"chapterLower"}
+                        duration={getDuration(idx)}
+                        chapterData={data[idx] || []}
+                        isShow={dataClickInfo[idx].isShow}
+                        isAnimation={dataClickInfo[idx].isAnimation}
+                        testHeight={80}
+                    />
                 </LevelItem>;
             })}
         </MenusStyle>
